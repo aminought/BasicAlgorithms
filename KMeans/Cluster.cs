27d1b8e997b477e49ecad0cd856327431668595d
aminought <aminought@gmail.com>
2015-12-31 04:17:59 +0500

    Implement k-means algorithm. Need to refactor

---
+++ KMeans/Cluster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KMeans
+{
+	public class Cluster
+	{
+		public List<Point> Points { get; set; }
+		public Point MeanPoint { get; set; }
+		public int Id { get; set; }
+
+		public Cluster (int id, Point random_mean_point)
+		{
+			Points = new List<Point> ();
+			Id = id;
+			MeanPoint = random_mean_point;
+		}
+	}
+}
+
---
+++ KMeans/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KMeans
+{
+	public class Point
+	{
+		public int X { get; set; }
+
+		public int Y { get; set; }
+
+		public Cluster Cluster { get; set; }
+
+		public Point (int rand_x, int rand_y)
+		{
+			X = rand_x;
+			Y = rand_y;
+		}
+
+		public double Distance (Point p)
+		{
+			return Math.Sqrt (Math.Pow (X - p.X, 2) + Math.Pow (Y - p.Y, 2));
+		}
+
+		public override bool Equals (object obj)
+		{
+			Point p = (Point)obj;
+			return p.X == X && p.Y == Y;
+		}
+
+	}
+}
