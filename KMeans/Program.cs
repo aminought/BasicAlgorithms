ca04e156a5544554e7d93d4fe689a050528eb3fb
aminought <aminought@gmail.com>
2015-12-31 05:18:10 +0500

    Refactor of KMeans

--- KMeans/Program.cs
+++ KMeans/Program.cs
@@ -3,85 +3,46 @@ using System.Collections.Generic;
 
 namespace KMeans
 {
-	class MainClass
+	class KMeans
 	{
+		private List<Point> Points { get; set; }
+
+		private List<Cluster> Clusters { get; set; }
+
 		public static int Main (string[] args)
 		{
-			const int width = 50;
-			const int height = 50;
-			List<Tuple<int, int>> table = new List<Tuple<int, int>> ();
-			for (int i = 0; i < width; ++i) {
-				for (int j = 0; j < height; ++j) {
-					table.Add (new Tuple<int, int> (i, j));
-				}
-			}
+			const int width = 100;
+			const int height = 100;
+			const int count_of_points = width * height;
+			const int count_of_clusters = 50;
 
-			const int count_of_points = 2500;
-			var points = new List<Point> ();
-			var rand = new Random ();
-			for (int i = 0; i < count_of_points; ++i) {
-				Tuple<int, int> rand_tuple = table [rand.Next (table.Count)];
-				var point = new Point (rand_tuple.Item1, rand_tuple.Item2);
-				table.Remove (rand_tuple);
-				points.Add (point);
-			}
+			var kmeans = new KMeans ();
+			kmeans.process (width, height, count_of_points, count_of_clusters);
+			Visualizer.Visualize (width, height, kmeans.Points);
+				
+			return 0;
+		}
 
-			const int count_of_custers = 100;
-			var clusters = new List<Cluster> ();
+		public KMeans ()
+		{
+			Points = new List<Point> ();
+			Clusters = new List<Cluster> ();
+		}
+
+		public void process (int width, int height, int count_of_points, int count_of_clusters)
+		{
+			var table = CreateTableForUniquePoints (width, height);
+			GenerateRandomPoints (count_of_points, table);
 			var old_mean_points = new List<Point> ();
-			for (int i = 0; i < count_of_custers; ++i) {
-				var random_mean_point = points [rand.Next (points.Count)];
-				Cluster cluster = new Cluster (i, random_mean_point);
-				cluster.Points.Add (random_mean_point);
-				random_mean_point.Cluster = cluster;
-				clusters.Add (cluster);
-				old_mean_points.Add (random_mean_point);
-			}
+			GenerateRandomClusters (count_of_clusters, old_mean_points);
 
 			bool isContinue = true;
 			do {
-				points.ForEach ((p) => {
-					var distances = new SortedDictionary<double, Point> ();
-					clusters.ForEach ((c) => {
-						try {
-							distances.Add (p.Distance (c.MeanPoint), c.MeanPoint);
-						} catch (ArgumentException) {
-							// Nothing to do
-						}
-					});
-					SortedDictionary<double, Point>.ValueCollection.Enumerator enumerator = distances.Values.GetEnumerator ();
-					enumerator.MoveNext ();
-					p.Cluster = enumerator.Current.Cluster;
-					p.Cluster.Points.Add (p);
-				});
-
+				SplitPointsIntoClusters ();
 				var new_mean_points = new List<Point> ();
-				clusters.ForEach ((c) => {
-					int sumX = 0;
-					int sumY = 0;
-					c.Points.ForEach ((p) => {
-						sumX += p.X;
-						sumY += p.Y;
-					});
-					Point central_point = new Point (sumX / c.Points.Count, sumY / c.Points.Count);
-					var distances = new SortedDictionary<double, Point> ();
-					c.Points.ForEach ((p) => {
-						try {
-							distances.Add (p.Distance (central_point), p);
-						} catch (ArgumentException) {
-							// Nothing to do
-						}
-					});
-					SortedDictionary<double, Point>.ValueCollection.Enumerator enumerator = distances.Values.GetEnumerator ();
-					enumerator.MoveNext ();
-					Point new_mean_point = enumerator.Current;
-					c.MeanPoint = new_mean_point;
-					new_mean_points.Add (new_mean_point);
-				});
-				visualize (width, height, points);
-				Console.WriteLine ();
+				ComputeNewMeanPoints (new_mean_points);
 
-				for (int i = 0; i < clusters.Count; ++i) {
+				for (int i = 0; i < Clusters.Count; ++i) {
 					if (!old_mean_points [i].Equals (new_mean_points [i])) {
 						break;
 					} else {
@@ -92,32 +53,85 @@ namespace KMeans
 					old_mean_points = new_mean_points;
 				}
 			} while(isContinue);
-
-			return 0;
 		}
 
-		public static void visualize (int width, int height, List<Point> points)
+		static List<Tuple<int, int>> CreateTableForUniquePoints (int width, int height)
 		{
-			char[,] field = new char[width, height];
-			for (int x = 0; x < width; ++x) {
-				for (int y = 0; y < height; ++y) {
-					Point point = points.Find ((p) => p.X == x && p.Y == y);
-					if (point != null) {
-						Console.ForegroundColor = ChooseColor (point.Cluster.Id);
-						Console.Write ("*");
-						Console.ResetColor ();
-					} else {
-						Console.Write (" ");
-					}
+			List<Tuple<int, int>> table = new List<Tuple<int, int>> ();
+			for (int i = 0; i < width; ++i) {
+				for (int j = 0; j < height; ++j) {
+					table.Add (new Tuple<int, int> (i, j));
 				}
-				Console.WriteLine ();
+			}
+			return table;
+		}
+
+		void GenerateRandomPoints (int count_of_points, List<Tuple<int, int>> table)
+		{
+			var rand = new Random ();
+			for (int i = 0; i < count_of_points; ++i) {
+				Tuple<int, int> rand_tuple = table [rand.Next (table.Count)];
+				var point = new Point (rand_tuple.Item1, rand_tuple.Item2);
+				table.Remove (rand_tuple);
+				Points.Add (point);
 			}
 		}
 
-		public static ConsoleColor ChooseColor (int n)
+		void GenerateRandomClusters (int count_of_clusters, List<Point> old_mean_points)
 		{
-			Array colors = Enum.GetValues (typeof(ConsoleColor));
-			return (ConsoleColor)colors.GetValue (n % (colors.Length - 1) + 1);
+			var rand = new Random ();
+			for (int i = 0; i < count_of_clusters; ++i) {
+				var random_mean_point = Points [rand.Next (Points.Count)];
+				Cluster cluster = new Cluster (i, random_mean_point);
+				cluster.Points.Add (random_mean_point);
+				random_mean_point.Cluster = cluster;
+				Clusters.Add (cluster);
+				old_mean_points.Add (random_mean_point);
+			}
+		}
+
+		void SplitPointsIntoClusters ()
+		{
+			Points.ForEach (p => {
+				var distances = new SortedDictionary<double, Point> ();
+				Clusters.ForEach (c => {
+					try {
+						distances.Add (p.Distance (c.MeanPoint), c.MeanPoint);
+					} catch (ArgumentException) {
+						// Nothing to
+					}
+				});
+				SortedDictionary<double, Point>.ValueCollection.Enumerator enumerator = distances.Values.GetEnumerator ();
+				enumerator.MoveNext ();
+				p.Cluster = enumerator.Current.Cluster;
+				p.Cluster.Points.Add (p);
+			});
+		}
+
+		void ComputeNewMeanPoints (List<Point> new_mean_points)
+		{
+			Clusters.ForEach (c => {
+				int sumX = 0;
+				int sumY = 0;
+				c.Points.ForEach (p => {
+					sumX += p.X;
+					sumY += p.Y;
+				});
+				Point central_point = new Point (sumX / c.Points.Count, sumY / c.Points.Count);
+				var distances = new SortedDictionary<double, Point> ();
+				c.Points.ForEach (p => {
+					try {
+						distances.Add (p.Distance (central_point), p);
+					} catch (ArgumentException) {
+						// Nothing to do
+					}
+				});
+				SortedDictionary<double, Point>.ValueCollection.Enumerator enumerator = distances.Values.GetEnumerator ();
+				enumerator.MoveNext ();
+				Point new_mean_point = enumerator.Current;
+				c.MeanPoint = new_mean_point;
+				new_mean_points.Add (new_mean_point);
+			});
 		}
 	}
 }
---
+++ KMeans/Visualizer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KMeans
+{
+	public static class Visualizer
+	{
+		public static void Visualize (int width, int height, List<Point> points)
+		{
+			char[,] field = new char[width, height];
+			for (int x = 0; x < width; ++x) {
+				for (int y = 0; y < height; ++y) {
+					Point point = points.Find ((p) => p.X == x && p.Y == y);
+					if (point != null) {
+						Console.ForegroundColor = ChooseColor (point.Cluster.Id);
+						Console.Write ("*");
+						Console.ResetColor ();
+					} else {
+						Console.Write (" ");
+					}
+				}
+				Console.WriteLine ();
+			}
+		}
+
+		public static ConsoleColor ChooseColor (int n)
+		{
+			Array colors = Enum.GetValues (typeof(ConsoleColor));
+			return (ConsoleColor)colors.GetValue (n % (colors.Length - 1) + 1);
+		}
+	}
+}
+
