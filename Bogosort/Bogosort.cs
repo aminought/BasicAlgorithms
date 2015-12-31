bba2de45fdebaf35b9fbf267352d2cbc49332cf5
aminought <aminought@gmail.com>
2015-12-31 20:02:05 +0500

    Add Bogosort

---
+++ Bogosort/Bogosort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Bogosort
+{
+	public class Bogosort
+	{
+		public List<int> Sort (List<int> elements)
+		{
+			var sorted_list = new List<int> (elements);
+			var rand = new Random ();	
+			while (!check (sorted_list)) {
+				sorted_list = sorted_list.OrderBy (e => rand.Next ()).ToList<int> ();
+			}
+			return sorted_list;
+		}
+
+		private bool check (List<int> elements)
+		{
+			for (int i = 1; i < elements.Count; ++i) {
+				int first = elements [i - 1];
+				int second = elements [i];
+				if (second < first) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
+
---
+++ Bogosort/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bogosort
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var bogosort = new Bogosort ();
+			var rand = new Random ();
+			var elements = Enumerable.Range (0, 10).OrderBy (e => rand.Next ()).ToList<int> ();
+			elements.ForEach (e => Console.Write (e + " "));
+			elements = bogosort.Sort (elements);
+			Console.WriteLine ();
+			elements.ForEach (e => Console.Write (e + " "));
+		}
+	}
+}
